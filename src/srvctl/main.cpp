3d053d9c946c7928ebf246d98faee3546565d4df
Alexander Filippov <a.filippov@yadro.com>
2022-12-14 14:53:35 +0300

    srvctl: Initial dummy version
    `srvctl` provides an interface for managing of some BMC services via
    D-Bus.
    
    This brings an initial dummy version of the tool.
    
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

---
+++ src/srvctl/main.cpp
@@ -0,0 +1,54 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2022, KNS Group LLC (YADRO).
+ */
+#include "config.h"
+
+#include <CLI/CLI.hpp>
+
+#include <cstdio>
+#include <stdexcept>
+
+static void setState(const std::string& /*service*/, bool /*state*/)
+{
+    throw std::runtime_error("Not implemented yet");
+}
+
+static void showList()
+{
+    throw std::runtime_error("Not implemented yet");
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    CLI::App app("");
+
+    auto listCmd = app.add_subcommand(
+        "list", "List available services and their actual state");
+    listCmd->callback(showList);
+
+    std::string service;
+
+    auto enableCmd =
+        app.add_subcommand("enable", "Enable a particular service");
+    enableCmd->add_option("SERVICE", service, "Service name")->required();
+    enableCmd->callback([&]() { setState(service, true); });
+
+    auto disableCmd =
+        app.add_subcommand("disable", "Disable a particular service");
+    disableCmd->add_option("SERVICE", service, "Service name")->required();
+    disableCmd->callback([&]() { setState(service, false); });
+
+    app.require_subcommand(1);
+    try
+    {
+        CLI11_PARSE(app, argc, argv);
+    }
+    catch (const std::exception& e)
+    {
+        fprintf(stderr, "ERROR: %s\n", e.what());
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
